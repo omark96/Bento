331468ebe8c4777595d88747a86e44c8c0a065cf
omark96 <43138339+omark96@users.noreply.github.com>
2021-11-25 00:26:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -23,14 +23,14 @@ const CONFIG = {
   greetingNight: 'Go to Sleep!',
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456',
+  weatherKey: '55016f55f0e14b15b8fafaa7340ed081',
   weatherIcons: 'Nord', // 'Nord', 'Dark', 'White'
   weatherUnit: 'C', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: '37.775',
-  defaultLongitude: '-122.419',
+  defaultLatitude: '358.348980',
+  defaultLongitude: '11.938330',
 
   // ┌─┐┌─┐┬─┐┌┬┐┌─┐
   // │  ├─┤├┬┘ ││└─┐
